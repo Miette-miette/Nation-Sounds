3bf183406366d875c00c12d04be7821bf5611305
Miette-miette <127025783+Miette-miette@users.noreply.github.com>
2024-05-04 18:46:52 +0200

    page reseaux sociaux

--- script/menu.js
+++ script/menu.js
@@ -4,16 +4,21 @@ console.log(menuBtn);
 let menuCollapse=document.getElementById("menuInfo");
 console.log(menuCollapse);
 
+let menuImg=document.getElementById("iconMenu");
+console.log(menuImg);
+
 function showMenu(){
 
     menuBtn.addEventListener('click', ()=>{
 
         if(menuCollapse.style.display==="flex"){
             menuCollapse.style.display="none";
+            menuImg.src="../media/icons/list.svg"
             console.log(menuCollapse);
         }
         else{
-            menuCollapse.style.display="flex";  
+            menuCollapse.style.display="flex";
+            menuImg.src="./media/icons/x.svg"  
             console.log(menuCollapse);
         }
     })
