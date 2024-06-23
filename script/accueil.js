2e9219143e752ee1904035ce75d1f16359316a68
Doriane <127025783+Miette-miette@users.noreply.github.com>
2024-06-23 23:00:23 +0200

    Update accueil.js

--- script/accueil.js
+++ script/accueil.js
@@ -1,5 +1,5 @@
-import CMS from './script/CMS.js';
-import fetchRessource from './script/fetchressource.js';
+import CMS from '/CMS.js';
+import fetchRessource from '/fetchressource.js';
 
 let cms= new CMS(); 
 
